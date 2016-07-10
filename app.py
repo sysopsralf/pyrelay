3e5b9fcd1d050a9a6bfe57e7f5518d563a2ce0d7
sysopsralf <rafael.gomez@avemaria.edu>
2016-07-10 00:07:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Test publishing device data via MQTT to relayr cloud.
-
-This subscribes to a given MQTT topic and publishes messages
-for this topic, so it receives the same messages previously
-posted.
-
-This code needs the paho-mqtt package to be installed, e.g.
-with "pip install paho-mqtt>=1.1".
-"""
-
+from flask import Flask
 import json
 import time
-
 import paho.mqtt.client as mqtt
-
-
-# mqtt credentials
 creds = {
     'clientId': 'TT2DMJUPgQIeeHudUeT9UPg',
     'user':     '4f60cc25-43e0-4087-9e1e-e754793f543e',
@@ -27,13 +10,15 @@ creds = {
     'server':   'mqtt.relayr.io',
     'port':     1883
 }
-
-
 # ATTENTION !!!
 # DO NOT try to set values under 200 ms of the server
 # will kick you out
 publishing_period = 1000
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!!"
 
 class MqttDelegate(object):
     "A delegate class providing callbacks for an MQTT client."
@@ -43,15 +28,15 @@ class MqttDelegate(object):
         self.credentials = credentials
 
     def on_connect(self, client, userdata, flags, rc):
-        print('Connected.')
+        return "Connected."
         # self.client.subscribe(self.credentials['topic'].encode('utf-8'))
         self.client.subscribe(self.credentials['topic'] + 'cmd')
 
     def on_message(self, client, userdata, msg):
-        print('Command received: %s' % msg.payload)
+        return "Command received: %s' % msg.payload"
 
     def on_publish(self, client, userdata, mid):
-        print('Message published.')
+        return "Message published."
 
 
 def main(credentials, publishing_period):
@@ -65,7 +50,7 @@ def main(credentials, publishing_period):
     # client.tls_set(cafile)
     # client.tls_insecure_set(False)
     try:
-        print('Connecting to mqtt server.')
+        return "Connecting to mqtt server."
         server, port = credentials['server'], credentials['port']
         client.connect(server, port=port, keepalive=60)
     except:
@@ -89,6 +74,6 @@ def main(credentials, publishing_period):
         time.sleep(publishing_period / 1000.)
 
 
-if __name__ == '__main__':
-    main(creds, publishing_period)
+
+if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
